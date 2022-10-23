4ba9b123fd813c1775e810206c6d50fb0aeac15a
Mayar192785 <96396572+Mayar192785@users.noreply.github.com>
2022-10-23 21:10:47 +0200

    Final Commit

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-let number0= document.querySelector(".operand0");
-let number1= document.querySelector(".operand1");
-let number2= document.querySelector(".operand2");
-let number3= document.querySelector(".operand3");
-let number4= document.querySelector(".operand4");
-let number5= document.querySelector(".operand5");
-let number6= document.querySelector(".operand6");
-let number7= document.querySelector(".operand7");
-let number8= document.querySelector(".operand8");
-let number9= document.querySelector(".operand9");
+const number0= document.querySelector(".operand0");
+const number1= document.querySelector(".operand1");
+const number2= document.querySelector(".operand2");
+const number3= document.querySelector(".operand3");
+const number4= document.querySelector(".operand4");
+const number5= document.querySelector(".operand5");
+const number6= document.querySelector(".operand6");
+const number7= document.querySelector(".operand7");
+const number8= document.querySelector(".operand8");
+const number9= document.querySelector(".operand9");
 
-let addd= document.querySelector(".add");
-let subtractt= document.querySelector(".subtract");
-let Multi= document.querySelector(".multi");
-let dividee= document.querySelector(".divide");
-let decimal= document.querySelector(".decimal");
-let equal= document.querySelector(".equal");
-let clearr=document.querySelector(".clear");
-let backspace=document.querySelector(".backspace");
-let screen=document.getElementById("result");
+
+const addd= document.querySelector(".add");
+const subtractt= document.querySelector(".subtract");
+const multi= document.querySelector(".multi");
+const dividee= document.querySelector(".divide");
+const decimal= document.querySelector(".decimal");
+const equal= document.querySelector(".equal");
+const clearr=document.querySelector(".clear");
+const backspace=document.querySelector(".backspace");
+const screen=document.getElementById("result");
 
 let text="";
-let result="";
+let result=0;
 let operationState="";
 const fb=undefined;
 let equation=fb||[];
 let k=0;
 
 const numbers=[number0,number1,number2,number3,number4,number5,
-              number6,number7,number8,number9,addd,subtractt,
-              Multi,dividee,equal,decimal];
-
-
+               number6,number7,number8,number9,addd,subtractt,
+               multi,dividee,equal,decimal];
 
 
 for(let i=0; i<numbers.length; i++)
@@ -45,8 +44,7 @@ for(let i=0; i<numbers.length; i++)
                 if(equation.includes("+") || equation.includes("-")||equation.includes("*")||equation.includes("/"))
                 {
                     displayclear();
-                    display(text);
-                    
+                    display(text);  
                 }
                 if(numbers[i].value==".")
                 {
@@ -60,61 +58,58 @@ for(let i=0; i<numbers.length; i++)
            
             if(numbers[i].value=="+"||numbers[i].value=="-"||numbers[i].value=="*"||numbers[i].value=="/")
             {
-                equation.push(text);
+                if(text==""){
+                    equation.push(numbers[i].value);
+                }
+                else{
+                    equation.push(text);
+                    decimal.disabled=false;
+                    number0.disabled=false;
+                    equation.push(numbers[i].value);
+                }
+               
                 text="";
-                decimal.disabled=false;
-                number0.disabled=false;
-                equation.push(numbers[i].value);
-                console.log(equation);
                 
             }
-            else if(numbers[i].value=="=")
-            {
-
-                if(equation.includes("*"))
-                {
+            else if(numbers[i].value=="="){
                     equation.push(text);
-                    k=equation.indexOf("*");
-                    result= multiply(parseFloat(equation[k-1]),parseFloat(equation[k+1]));
-                    equation.splice(k-1,3,result);
-                    displayclear();
-                    console.log(equation);
-                }
-                else if(equation.includes("/"))
-                {
-                    equation.push(text);
-                    k=equation.indexOf("/");
-                    result= divide(parseFloat(equation[k-1]),parseFloat(equation[k+1]));
-                    equation.splice(k-1,3,result);
-                    displayclear();
-                    console.log(equation);
-                }
-                else if(equation.includes("+")) 
-                {
-                    equation.push(text);
-                    k=equation.indexOf("+");
-                    result= add(parseFloat(equation[k-1]),parseFloat(equation[k+1]));
-                    equation.splice(k-1,3,result);
-                    displayclear();
-                    console.log(equation);
-                }
-                else if(equation.includes("-"))
-                {
-                    equation.push(text);
-                    k=equation.indexOf("-");
-                    result= subtract(parseFloat(equation[k-1]),parseFloat(equation[k+1]));
-                    equation.splice(k-1,3,result);
-                    displayclear();
-                    console.log(equation);
-                }
-                display(result);  
-           }
-         
+                    for(let j=0;j<equation.length;j++){
+                        if(equation.includes("*"))
+                        {
+                            k=equation.indexOf("*");
+                            result= multiply(parseFloat(equation[k-1]),parseFloat(equation[k+1]));
+                            equation.splice(k-1,3,result);
+                        }
+                        else if(equation.includes("/"))
+                        {
+                            k=equation.indexOf("/");
+                            result= divide(parseFloat(equation[k-1]),parseFloat(equation[k+1]));
+                            if(Number.isInteger(result)==false){
+                                result=result.toFixed(10);
+                            }
+                            equation.splice(k-1,3,result);
+                        }
+                        else if(equation.includes("+")) 
+                        {
+                            k=equation.indexOf("+");
+                            result= add(parseFloat(equation[k-1]),parseFloat(equation[k+1]));
+                            equation.splice(k-1,3,result);
+                        }
+                        else if(equation.includes("-"))
+                        {
+                            k=equation.indexOf("-");
+                            result= subtract(parseFloat(equation[k-1]),parseFloat(equation[k+1]));
+                            equation.splice(k-1,3,result);
+                        }
+                    }
+                
+                displayclear(); 
+                text="";  
+                display(result); 
+            }
     });   
 }
 
-
-    
 const add= (fnumber,snumber) =>{
     return fnumber+snumber;
 }
